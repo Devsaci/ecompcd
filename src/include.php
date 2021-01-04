6402819b9e812d5b0c30f655211e718606a55aef
saci zakaria <sacideveloppeur@gmail.com>
2021-01-04 17:03:16 +0100

    //Gestionnaire de requettes

--- src/include.php
+++ src/include.php
@@ -6,7 +6,7 @@ define("SP", DIRECTORY_SEPARATOR);
 define("CONFIG", ROOT.SP."config"); 
 define("VIEWS", ROOT.SP."views"); 
 define("MODEL", ROOT.SP."model");
-print_r(array(CONFIG,MODEL,VIEWS));
+//print_r(array(CONFIG,MODEL,VIEWS));
 
 //print_r(SRC);//exit();
 // les fonctions appelée par le controller
