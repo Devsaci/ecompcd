e3317abddc845434556aabb68af70f39cbdc95c2
saci zakaria <sacideveloppeur@gmail.com>
2021-01-04 17:49:19 +0100

    Aucun affichage

--- src/functions.php
+++ src/functions.php
@@ -1,7 +1,10 @@
 <?php
- function displayAccueil(){
-    return '<h1> Bienvenu sur la page d\'Accueil</h1>';
 
- }
+function displayAccueil(){
+    return '<h1> Bienvenu sur la page d\'Accueil</h1>';
+}
+function displayContact(){
+    return '<h1> Bienvenu sur la page de contact</h1>';
+}
 
 ?>
