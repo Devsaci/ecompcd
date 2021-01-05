cb9139d4538908c70b5cdb9f5f8f58f2485689f6
saci zakaria <sacideveloppeur@gmail.com>
2021-01-05 10:35:41 +0100

    276 implementation du model

--- model/DataLayer.class.php
+++ model/DataLayer.class.php
@@ -5,7 +5,7 @@ function __construct(){
 
     try {
         $this->connexion = new PDO("mysql:host=".HOST.";dbname=".DB_NAME,DB_USER,DB_PASSWORD);
-        echo "connection Ok";
+        //echo "connection Ok";
     } catch (PDOException $th) {
         echo $th->getMessage();
     }
