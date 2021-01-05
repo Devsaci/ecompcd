4bdfbf6a8a36a22bf9293720336959a5bffbae81
saci zakaria <sacideveloppeur@gmail.com>
2021-01-05 09:25:32 +0100

    275 Mis en place du model du site :

--- src/index.php
+++ src/index.php
@@ -4,25 +4,25 @@
 //Gestionnaire de requettes
 require "include.php";
 
-$url = trim($_SERVER['PATH_INFO'],'/');
-$url = explode('/',$url);
+$url = trim($_SERVER['PATH_INFO'], '/');
+$url = explode('/', $url);
 $route = array("accueil", "contact");
 //print_r($url);
 $action = $url[0];
 // print_r($action);
 
 // controller
-if (!in_array($action,$route)) {
+if (!in_array($action, $route)) {
     $title = "Page Error";
     $content = "URL introuvable !";
 } else {
     //echo "Bienvenue dans la page   ".$action ;
-    $function = "display".ucwords($action);
-    $title = "Page ".$action;
+    $function = "display" . ucwords($action);
+    $title = "Page " . $action;
     $content = $function();
     //echo $function;
-    require VIEWS.SP."templates".SP."default.php";
 }
+require VIEWS . SP . "templates" . SP . "default.php";
 ?>
 
 
