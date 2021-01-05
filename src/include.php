9344353d2bc278d0e41a095f42469ad04a8b0c35
saci zakaria <sacideveloppeur@gmail.com>
2021-01-05 10:03:08 +0100

    => connection OK

--- src/include.php
+++ src/include.php
@@ -1,14 +1,22 @@
 <?php
 //definition des constante
 define("SRC", dirname(__FILE__));
+//print_r(SRC);//exit();
 define("ROOT", dirname(SRC));
 define("SP", DIRECTORY_SEPARATOR);
-define("CONFIG", ROOT.SP."config"); 
 define("VIEWS", ROOT.SP."views"); 
 define("MODEL", ROOT.SP."model");
+define("CONFIG", ROOT.SP."config"); 
 //print_r(array(CONFIG,MODEL,VIEWS));
 
-//print_r(SRC);//exit();
+// import du model
+require CONFIG.SP."config.php";
+require MODEL.SP."DataLayer.class.php";
+// Instanciation de DataLayer
+$data = new DataLayer();exit();
+
+
+
 // les fonctions appelée par le controller
 require "functions.php";
 
